b10d9b34f9794b49c3429b3d1e1d1d9b5590f04b
Mr.恒尘 <rainstorm.me@outlook.com>
2020-06-10 09:36:56 +0800

    [U] oracle-jdk: 删除参数校验，简化shell脚本 && 构建镜像时拉取JDK压缩包

--- jdk/oracle/build.sh
+++ jdk/oracle/build.sh
@@ -1,62 +1,17 @@
 #!/bin/bash
 
-# ./build.sh --tag=1.8.0_191
+# ./build.sh --build-arg HTTPS_PROXY=https://host.docker.internal:1087
 
 # 华为云 JDK镜像 https://mirrors.huaweicloud.com/java/jdk/
 
-function usage() {
-    echo "./build.sh [option]"
-    echo ""
-    echo "[option]"
-    echo ""
-    echo "-t --tag=[value]   用于指定构建 JDK的tag"
-    echo "                   不传默认使用不带后缀的文件名"
-    echo "-h --help          帮助文档"
-    echo "                   "
-    echo "[Example]          "
-    echo "                   "
-    echo "tag=v1.8.0_191"
-    echo "  ./build.sh --tag=v1.8.0_191"
-    echo "  ./build.sh -tv1.8.0_191"
-}
+CENTOS7_DIR=../../os/centos/7
+JDK_ROOT=`pwd`
 
-tag=""
-while getopts t:h-: opt;do
-    case $opt in
-        -)
-            case $OPTARG in
-                help)
-                    usage
-                    exit 0
-                    ;;
-                tag=*)
-                    tag=${OPTARG#*=}
-                    ;;
-            esac
-            ;;
-        t)
-            tag=$OPTARG
-            ;;
-        h)
-            usage
-            exit 0
-            ;;
-        \?)
-            usage
-            exit 1
-            ;;
-    esac
-done
-
-if [ `ls | grep jdk |wc -l` -gt 0 ]; then
-    export ZIP_FILE=`ls | grep jdk`
-    echo "jdk found: ${ZIP_FILE}"
-
-    if [ ${#tag} -eq 0 ]; then
-        tag=`echo ${ZIP_FILE} | awk -F. '{print $1}'`
-    fi
-
-    docker build --build-arg JDK_ZIP_FILE=${ZIP_FILE} -t=rainstorm/oracle-jdk:${tag} .
-else
-    echo "jdk 不存在，可以到 https://mirrors.huaweicloud.com/java/jdk/ 下载"
+if [ `docker images rainstorm/custom-centos:7 -q | wc -l` -eq 0 ]; then
+    echo "Local rainstorm/custom-centos:7 not found, try to build"
+    echo "Starting build rainstorm/custom-centos:7"
+    cd ${CENTOS7_DIR} && ./build.sh --build-arg HTTPS_PROXY=https://host.docker.internal:1087 && cd ${JDK_ROOT}
+    echo "done"
 fi
+
+docker build `echo $@` -t=rainstorm/oracle-jdk:1.8.0_191 .
